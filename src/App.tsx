2d8d1a697fb4b70e090f71f00fbe4680363d9ae4
Mayplex <andrew.yakubovski.1@gmail.com>
2023-04-08 17:09:27 +0200

    rm unecessary imports and fix prettier issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
 import { Todolist } from "./Todolist";
-import { TasksType } from "./Todolist";
 
 export type FilteredValueType = "all" | "completed" | "active";
 
